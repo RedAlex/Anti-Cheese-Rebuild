d99d5d356f76942010c32a3fb5917b9da4b5f0c4
Alex Garcio <redalex24@gmail.com>
2019-10-02 19:40:33 -0400

    Add config

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config                   = {}
+
+--WEBHOOK
+Config.EnableDiscordLink = true -- only turn this on if you want link the log to a discord
+Config.webhook           = ""
+
+--Temp info
+Config.green             = 56108
+Config.grey              = 8421504
+Config.red               = 16711680
+Config.orange            = 16744192
+Config.blue              = 2061822
+Config.purple            = 11750815
